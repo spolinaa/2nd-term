0f0a66dc939562d2d6eff17fb052ebd1bf0106ea
spolinaa <sokolova.polina@mail.ru>
2015-02-22 15:40:02 +0300

    Renamed

--- Peano_Trees/tree.fs
+++ Peano_Trees/tree.fs
@@ -1,5 +1,5 @@
 ﻿(* Some operations (insert, remove, print) with binary search trees
-(expectation: 4 h; reality: 3,5 h)
+(expectation: 4 h; reality: 4 h)
 by Sokolova Polina *)
 
 type Tree = Nil | Node of int * Tree * Tree
@@ -29,11 +29,11 @@ let rec remove a t =
     | Node(c, L, R) -> if a < c then Node(c, remove a L, R)
                        elif a > c then Node(c, L, remove a R)
                        else match (L, R) with 
-                            | Nil, Nil -> Nil
-                            | L  , Nil -> L
-                            | Nil, R   -> R
-                            | L  , R   -> if L = Nil then Node(findInRight R, L, remove (findInRight R) R)
-                                          else Node(findInLeft L, remove (findInLeft L) L, R)
+                            | Nil, Nil           -> Nil
+                            | L  , Nil           -> L
+                            | Nil, R             -> R
+                            | L  , Node(C, l, r) -> if l = Nil then Node(findInLeft L, remove (findInLeft L) L, R)
+                                                    else Node(findInRight R, L, remove (findInRight R) R)
 let rec CLRprint t =
     match t with
     | Nil           -> printf ""
@@ -58,19 +58,34 @@ let rec LRCprint t =
 
 [<EntryPoint>]
 let main args =
-    let myTree = Node(8, Node(3, Node(1, Nil, Nil), Node(6, Node(4, Nil, Nil), Node(7, Nil, Nil))), Node(10, Nil, (Node(14, Node(13, Nil, Nil), Nil))))
+    let myTree = Node (8,Node(3,Node(1,Nil,Nil),Node(6,Node(4,Nil,Nil),Node(7,Nil,Nil))),Node(10,Nil,(Node(14,Node(13,Nil,Nil),Nil))))
+    printf "%A\n" myTree
+    printf "LCR: "
+    LCRprint myTree
+    printf "\nLRC: "
+    LRCprint myTree
+    printf "\nCLR: "
+    CLRprint myTree
+    printf "\n"
     let myTree = remove 8 myTree
-    LCRprint myTree
-    printf "\n"
+    printf "remove 8: %A\n" myTree
     let myTree = remove 7 myTree
-    LCRprint myTree
-    printf "\n"
+    printf "remove 7: %A\n" myTree
     let myTree = insert 9 myTree
-    LCRprint myTree
-    printf "\n"
+    printf "insert 9: %A\n" myTree
+    let myTree = insert 11 myTree
+    printf "insert 11: %A\n" myTree
     let myTree = remove 6 myTree
-    LCRprint myTree
-    printf "\n"
+    printf "remove 6: %A\n" myTree
     let myTree = remove 70 myTree
+    printf "remove 70: %A\n" myTree
+    printf "%A\n" myTree
+    printf "LCR: "
     LCRprint myTree
+    printf "\nLRC: "
+    LRCprint myTree
+    printf "\nCLR: "
+    CLRprint myTree
     printf "\n"
+    0
+
