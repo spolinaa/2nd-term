d893b67d97ac25b805a8654b757348f5d9523fc4
spolinaa <sokolova.polina@mail.ru>
2015-04-19 15:02:27 +0300

    Add workflow into master branch

---
+++ Workflow/src/app/RingWorkflow.fs
@@ -0,0 +1,18 @@
+﻿(* Workflow for ring
+(expectation: 2 h; reality: 1,5 h)
+by Sokolova Polina *)
+
+module RingWorkflow
+  
+type RingBuilder (n : int) =
+  member x.Bind (a, f) = (f a) % n
+  member x.Return a = 
+    let rec ret b =
+      if b < 0 then ret (b + n)
+      else b
+    ret a % n
+
+let ring n = new RingBuilder (n)
+
+
+
